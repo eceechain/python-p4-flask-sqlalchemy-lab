2b54cec8c9b33445ab2f6686b1291cff8eae6375
ecee <you@example.com>
2024-01-07 09:38:42 +0300

    Done with Assignment

--- server/app.py
+++ server/app.py
@@ -13,22 +13,42 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+def format_attributes(obj):
+    return [f"<li>{attr}: {getattr(obj, attr)}</li>" for attr in obj.__dict__ if not callable(getattr(obj, attr)) and not attr.startswith("_")]
+
+def create_response(obj_type, attributes):
+    return make_response(f"<h2>{obj_type}</h2><ul>{''.join(attributes)}</ul>", 200)
+
 @app.route('/')
 def home():
     return '<h1>Zoo app</h1>'
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    if animal:
+        response_body = format_attributes(animal)
+        return create_response("Animal", response_body)
+    else:
+        return make_response("Animal not found", 404)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    if zookeeper:
+        response_body = format_attributes(zookeeper)
+        return create_response("Zookeeper", response_body)
+    else:
+        return make_response("Zookeeper not found", 404)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    enclosure = Enclosure.query.get(id)
+    if enclosure:
+        response_body = format_attributes(enclosure)
+        return create_response("Enclosure", response_body)
+    else:
+        return make_response("Enclosure not found", 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,13 +11,25 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+    animals = db.relationship('Animal', back_populates='zookeeper')
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)  # Add this line
+    open_to_visitors = db.Column(db.Boolean)
+
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    species = db.Column(db.String, nullable=False)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+    enclosure = db.relationship('Enclosure', back_populates='animals')
